ce8f075ef17bd08384eb5c5df281d1f989934f14
Fellipe Augusto <Fellipe246@hotmail.com>
2025-12-16 16:15:22 -0300

    DepartmentController

--- obj/Debug/net10.0/SalesWeb.AssemblyInfo.cs
+++ obj/Debug/net10.0/SalesWeb.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SalesWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4e780edc683b471feb31e54f60cbc311b6eff43f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c81d7771fdd89bec31efc10a45a21beb4e7d2913")]
 [assembly: System.Reflection.AssemblyProductAttribute("SalesWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SalesWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
